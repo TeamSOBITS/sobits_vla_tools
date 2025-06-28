c0411841b363645c75c436030e516ccabfc6916a
MrKeith99 <kvalentincardenas@hotmail.com>
2025-06-28 21:29:19 +0900

    [UPD] implement action publisher, upd obs cb, add config for model

--- sobits_vla_deploy/sobits_vla_deploy/smol_vla_deploy.py
+++ sobits_vla_deploy/sobits_vla_deploy/smol_vla_deploy.py
@@ -58,18 +58,27 @@ class SmolVLADeploy(Node):
                 'topic': self.get_parameter(f'robot.cameras.{camera_type}.topic').get_parameter_value().string_value
             }
 
-        self.declare_parameter('model_path', 'team-sobits/sobit_light_smolvla')
-        model_path = self.get_parameter('model_path').get_parameter_value().string_value
-
         # Declare parameters for gamepad configuration
         self.declare_parameter('gamepad.topic', '/joy')
         self.gamepad_topic = self.get_parameter('gamepad.topic').get_parameter_value().string_value
         self.declare_parameter('gamepad.play_button', 0)  # Assuming the play button is at index 0
         self.play_button = self.get_parameter('gamepad.play_button').get_parameter_value().integer_value
 
+        # Declare parameters for model configuration
+        self.declare_parameter('model.repo_id', 'team-sobits/sobit_light_smolvla')
+        model_id = self.get_parameter('repo_id').get_parameter_value().string_value
+        self.declare_parameter('model.device', 'cuda')
+        model_device = self.get_parameter('model.device').get_parameter_value().string_value
+        self.declare_parameter('model.use_amp', True)
+        model_use_amp = self.get_parameter('model.use_amp').get_parameter_value().bool_value
+
         # Load policy
         self.policy = SmolVLAPolicy.from_pretrained(
-            model_path,
+            pretrained_name_or_path = model_id,
+            config= {
+                'device': model_device,
+                'use_amp': model_use_amp
+            }
         )
         self.policy.reset()
 
@@ -83,6 +92,11 @@ class SmolVLADeploy(Node):
         self.cam_vector = {camera_type: None for camera_type in self.camera_types}
         self.obs = None
         self.bridge = CvBridge()
+
+        self.body_joint_key2ros = {joint: ros_joint for joint, ros_joint in zip(self.robot_joints['body']['joints'], self.robot_joints['body']['joints_ros'])}
+
+        self.joint_states = None  # Placeholder for joint states
+        self.odom_states = None  # Placeholder for odometry data
 
         # Set QoS profile
         qos_profile = QoSProfile(
@@ -153,16 +167,63 @@ class SmolVLADeploy(Node):
             return
         
         # Get observation
-        
-        
+        self.obs = {**self.state_vector, **self.cam_vector}
+        self.get_logger().debug(f"Current observation: {self.obs}")
 
+        self.obs_frame = build_dataset_frame(self.obs, self.obs_ft, prefix="observation")
+        self.get_logger().debug(f"Observation frame: {self.obs_frame}")
+
+        # Predict action using the policy
+        self.act_values = predict_action(
+                self.obs_frame, self.policy, get_safe_torch_device(self.policy.config.device), self.policy.config.use_amp
+            )
+        self.get_logger().debug(f"Predicted action values: {self.act_values}")
+
+        # Create joint trajectory message
+        joint_trajectory = JointTrajectory()
+        joint_trajectory.joint_names = self.robot_joints['body']['joints_ros']
+        joint_trajectory_point = JointTrajectoryPoint()
+        
+        # Joint key to ROS joint name mapping
+        joint_key_values = {
+            ros_joint: self.act_values[joint_key]
+            for joint_key, ros_joint in self.body_joint_key2ros.items()
+        }
+
+        joint_trajectory_point.positions = [joint_key_values[ros_joint] for ros_joint in joint_trajectory.joint_names]
+        joint_trajectory_point.time_from_start.sec = 0
+        joint_trajectory_point.time_from_start.nanosec = 100000000  # 100ms
+        joint_trajectory.points.append(joint_trajectory_point)
+
+        # Publish joint trajectory
+        self.act_joint_pub.publish(joint_trajectory)
+        self.get_logger().debug(f"Published joint trajectory: {joint_trajectory}")
+
+        # Create and publish mobile base action
+        mobile_base_action = Twist()
+        mobile_base_action.linear.x = self.act_values['x.vel']
+        mobile_base_action.angular.z = self.act_values['theta.vel']
+        self.act_mobile_base_pub.publish(mobile_base_action)
+        self.get_logger().debug(f"Published mobile base action: linear.x={mobile_base_action.linear.x}, angular.z={mobile_base_action.angular.z}")
 
 
     def obs_joint_callback(self, msg: JointState):
         self.get_logger().info(f"Received joint states: {msg.name}")
+        self.joint_states = msg
+
+        for joint_name, joint_value in zip(msg.name, msg.position):
+            if joint_name in self.body_joint_key2ros:
+                ros_joint_name = self.body_joint_key2ros[joint_name]
+                self.state_vector[ros_joint_name] = joint_value
+                self.get_logger().info(f"Joint {ros_joint_name} value: {joint_value}")
+
 
     def obs_odometry_callback(self, msg: Odometry):
         self.get_logger().info(f"Received odometry data: {msg.twist.twist.linear.x}, {msg.twist.twist.angular.z}")
+        self.odom_states = msg.twist.twist
+
+        self.state_vector['x.vel'] = msg.twist.twist.linear.x
+        self.state_vector['theta.vel'] = msg.twist.twist.angular.z
 
     def obs_cam_callback(self, msg: Image, camera_type: str):
         self.get_logger().info(f"Received image from {camera_type} camera")
