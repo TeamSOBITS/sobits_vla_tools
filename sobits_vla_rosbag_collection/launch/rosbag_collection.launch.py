f471bf3e02edc75826d33e47ba85e16cd1f9f2a3
MrKeith99 <kvalentincardenas@hotmail.com>
2026-03-12 22:53:54 +0900

    [UPD] add args

--- sobits_vla_rosbag_collection/launch/rosbag_collection.launch.py
+++ sobits_vla_rosbag_collection/launch/rosbag_collection.launch.py
@@ -3,23 +3,35 @@ from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument, OpaqueFunction
 
 
-def generate_launch_description():
-    robot_name = "sobit_light" # Change this to the desired robot name
-
+def generate_launch_description_impl(context, *args, **kwargs):
+    robot_name = LaunchConfiguration('robot_name').perform(context)
+    pkg_share = get_package_share_directory("sobits_vla_rosbag_collection")
+    
     rosbag_config = os.path.join(
-        get_package_share_directory("sobits_vla_rosbag_collection"),
+        pkg_share,
         "config",
         "record_settings_" + robot_name + ".yaml",
     )
+
+    record_directory = LaunchConfiguration('record_directory').perform(context)
+    if not record_directory:
+        record_directory = os.path.join(pkg_share, 'rosbags')
+
+    parameters = [
+        rosbag_config,
+        {'rosbag_config.record_directory': record_directory}
+    ]
 
     rosbag_collection_node = Node(
         package="sobits_vla_rosbag_collection",
         executable="rosbag_collection_node",
         name="rosbag_collection_node",
         namespace=robot_name,
-        parameters=[rosbag_config],
+        parameters=parameters,
         output="screen",
     )
 
@@ -33,7 +45,22 @@ def generate_launch_description():
     )
 
 
-    return LaunchDescription([
+    return [
         rosbag_collection_node,
         gamepad_clt_node,
+    ]
+
+def generate_launch_description():
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'record_directory',
+            default_value='',
+            description='Absolute path to where the rosbags should be dumped. Defaults to <package_share>/rosbags.'
+        ),
+        DeclareLaunchArgument(
+            'robot_name',
+            default_value='',
+            description='Name of the robot to record rosbags for.'
+        ),
+        OpaqueFunction(function=generate_launch_description_impl)
     ])
