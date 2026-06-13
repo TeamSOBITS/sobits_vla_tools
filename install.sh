0d4f5bef19325cdcf7cc19a92e256d403535fa27
mrkeith99 <kvalentincardenas@hotmail.com>
2026-06-13 16:09:19 +0900

    feat(install): update install.sh script

--- install.sh
+++ install.sh
@@ -4,6 +4,34 @@ set -e
 
 echo "╔══╣ Install: SOBITS VLA TOOLS (STARTING) ╠══╗"
 
+# Keep track of the current directory
+DIR=`pwd`
+
+cd ../
+
+# Download required packages
+ros_packages=(
+    "sobits_interfaces"
+)
+
+# Clone all packages
+for ((i = 0; i < ${#ros_packages[@]}; i++)) {
+    echo "Clonning: ${ros_packages[i]}"
+    git clone --recurse-submodules -b $ROS_DISTRO-devel https://github.com/TeamSOBITS/${ros_packages[i]}
+
+    # Check if install.sh exists in each package
+    if [ -f ${ros_packages[i]}/install.sh ]; then
+        echo "Running install.sh in ${ros_packages[i]}."
+        cd ${ros_packages[i]}
+        bash install.sh
+        cd ..
+    fi
+}
+
+# Go back to previous directory
+cd ${DIR}
+
+
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 WORKSPACE_ROOT=$(cd "${SCRIPT_DIR}/.." && pwd)
 
