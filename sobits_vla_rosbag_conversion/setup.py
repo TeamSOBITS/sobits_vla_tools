21527ca08b4af03481ebe56b03c2ad63f07d5252
mrkeith99 <kvalentincardenas@hotmail.com>
2026-08-28 11:52:36 +0900

    fix(conversion): install lerobotdataset .gitignore into share
    .gitkeep was replaced by the .gitignore; the data_files glob follows so the
    dataset root exists in the install space.

--- sobits_vla_rosbag_conversion/setup.py
+++ sobits_vla_rosbag_conversion/setup.py
@@ -1,3 +1,31 @@
+# Copyright (c) 2026, Team SOBITS
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from this
+#   software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
 from glob import glob
 
 from setuptools import find_packages, setup
@@ -13,8 +41,11 @@ setup(
         (f'share/{package_name}', ['package.xml']),
         (f'share/{package_name}/launch', glob('launch/*.py')),
         (f'share/{package_name}/config',  glob('config/*.yaml')),
-        (f'share/{package_name}/lerobotdataset', glob('lerobotdataset/.gitkeep')),
+        (f'share/{package_name}/lerobotdataset', glob('lerobotdataset/.gitignore')),
     ],
+    # Standalone tool, not importable from the package dir: installed as a
+    # script (lib/<pkg>/ via setup.cfg), not a console_scripts entry point.
+    scripts=['scripts/visualize_ee_pose.py'],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='VALENTIN Keith',
